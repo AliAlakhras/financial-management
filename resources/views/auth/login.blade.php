f553288cdfef2501d60832cfe86dd83b88987ad7
Ezzeddin <dev.ezzeddin@gmail.com>
2019-09-01 12:47:13 +0300

    edit login page validation

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,10 +57,20 @@
                             <div class="form-group">
                                 <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
                                        required autocomplete="email" autofocus>
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message}}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="form-group">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password"
                                        required autocomplete="current-password">
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <!-- Change this to a button or input when using this as a form -->
                             <button type="submit" class="btn btn-primary">
